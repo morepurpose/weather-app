c68890175bc57094a2ddcdfc0128de4e061ce8e5
morepurpose <90328435+morepurpose@users.noreply.github.com>
2024-09-21 02:25:32 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,120 @@
+import "./style.css"
+
+const weatherForm = document.querySelector(".weatherForm");
+const cityInput = document.querySelector(".cityInput");
+const card = document.querySelector(".card");
+const apiKey = "cce327de7f701ed939b9c7c311bc8656";
+const toggleButton = document.getElementById("toggleButton");
+
+weatherForm.addEventListener("submit", async event => {
+    event.preventDefault();
+
+    const city = cityInput.value;
+
+    if (city) {
+        try {
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        } catch (error) {
+            console.error(error);
+            displayError(error);
+        }
+    } else {
+        displayError("Please enter a city");
+    }
+});
+
+async function getWeatherData(city) {
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+        throw new Error("Could not fetch weather data");
+    }
+
+    return await response.json();
+}
+
+function displayWeatherInfo(data) {
+    const { name: city, main: { temp, humidity }, weather: [{ description, id }] } = data;
+    card.textContent = "";
+    card.style.display = "flex";
+
+    const temperatureCelsius = kelvinToCelsius(temp);
+
+    const cityDisplay = document.createElement("h1");
+    const tempDisplay = document.createElement("p");
+    const humidityDisplay = document.createElement("p");
+    const descDisplay = document.createElement("p");
+    const weatherEmoji = document.createElement("p");
+
+    cityDisplay.textContent = city;
+    tempDisplay.textContent = `${temperatureCelsius.toFixed(2)}°C`; // Display in Celsius by default
+    humidityDisplay.textContent = `Humidity: ${humidity}%`;
+    descDisplay.textContent = description;
+    weatherEmoji.textContent = getWeatherEmoji(id);
+
+    cityDisplay.classList.add("cityDisplay");
+    tempDisplay.classList.add("tempDisplay");
+    humidityDisplay.classList.add("humidDisplay");
+    descDisplay.classList.add("descDisplay");
+    weatherEmoji.classList.add("weatherEmoji");
+
+    card.appendChild(cityDisplay);
+    card.appendChild(tempDisplay);
+    card.appendChild(humidityDisplay);
+    card.appendChild(descDisplay);
+    card.appendChild(weatherEmoji);
+
+    toggleButton.addEventListener("click", function() {
+        if (toggleButton.textContent === "°C") {
+            tempDisplay.textContent = `${temp.toFixed(2)}°K`;
+            toggleButton.textContent = "°K";
+        } else {
+            tempDisplay.textContent = `${temperatureCelsius.toFixed(2)}°C`;
+            toggleButton.textContent = "°C";
+        }
+    });
+}
+
+function kelvinToCelsius(kel) {
+    return kel - 273.15;
+}
+
+function celsiusToKelvin(celsius) {
+    return (celsius + 273.15);
+}
+
+function getWeatherEmoji(weatherId) {
+    switch (true) {
+        case (weatherId >= 200 && weatherId < 300):
+            return "⛈️";
+        case (weatherId >= 300 && weatherId < 400):
+            return "🌧️";
+        case (weatherId >= 400 && weatherId < 500):
+            return "🌧️";
+        case (weatherId >= 500 && weatherId < 600):
+            return "🌧️";
+        case (weatherId >= 600 && weatherId < 700):
+            return "🌨️";
+        case (weatherId >= 700 && weatherId < 800):
+            return "🌫️";
+        case (weatherId === 800):
+            return "☀️";
+        case (weatherId >= 801 && weatherId < 810):
+            return "☁️";
+        default:
+            return "❓";
+    }
+}
+
+function displayError(message) {
+    const errorDisplay = document.createElement("p");
+    errorDisplay.textContent = message;
+    errorDisplay.classList.add("errorDisplay");
+
+    card.textContent = "";
+    card.style.display = "flex";
+    card.appendChild(errorDisplay);
+}
